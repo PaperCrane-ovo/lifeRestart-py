b7edcc29117467e643480cf97350204450f550c9
Crane <15352461680@126.com>
2023-04-27 13:18:05 +0800

    增加了q键退出

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ from TalentManager import TalentManager
 
 Life.load('data')
 
+
 def genp(prop):
     ps = []
     for i in range(3):
@@ -16,15 +17,16 @@ def genp(prop):
             ps[-1] = 10
         prop -= ps[-1]
     if(10 < prop):
-        prop+=sum(ps)
+        prop += sum(ps)
         ps = [int(prop / 4)] * 3
-        prop-=sum(ps)
+        prop -= sum(ps)
     return {
         'CHR': ps[0],
         'INT': ps[1],
         'STR': ps[2],
         'MNY': prop
     }
+
 
 def run():
     life = Life()
@@ -38,22 +40,24 @@ def run():
     life.talent.talents.append(TalentManager.talentDict[1048])
     life.talent.talents.append(TalentManager.talentDict[1065])
     life.talent.talents.append(TalentManager.talentDict[1134])
-    
-    #life.event.triggered.add(40063)
+
+    # life.event.triggered.add(40063)
 
     for t in life.talent.talents:
         print(t)
 
     return life.run()
 
+
 while True:
     i = 0
     for x in run():
-        print(f'\n{x[0]}{"——".join(x[1:])}',end='',flush=True)
+        print(f'\n{x[0]}{"——".join(x[1:])}', end='', flush=True)
         if(0 < i):
-            i-=1
+            i -= 1
             continue
         if(msvcrt.getch() == b' '):
             i = 9
     print("\n\n[人生重开了]")
-    msvcrt.getch()
+    if (msvcrt.getch() == b'q'):
+        break
